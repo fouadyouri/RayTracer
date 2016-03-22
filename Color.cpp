2cf1a8b475354c3a94891899d1a58f4aea36292d
Fouad B <fouad709@gmail.com>
2016-03-22 19:09:19 +0300

    Create Color.cpp

---
+++ Color.cpp
@@ -0,0 +1,30 @@
+#include "Color.h"
+
+
+// ======== Returnes RGB Colors ========
+double Color::getColorRed() {
+	return red;
+}
+
+double Color::getColorGreen() {
+	return green;
+}
+
+double Color::getColorBlue() {
+	return blue;
+}
+
+
+// ========== Sets RGB Colors ==========
+void Color::setColorRed(double r) {
+	red = r;
+}
+
+void Color::setColorGreen(double g) {
+	green = g;
+}
+
+void Color::setColorBlue(double b) {
+	blue = b;
+}
+
