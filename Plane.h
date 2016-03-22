4509fe5e0d6e664e5993edf2fefa9cb9889ffbd4
Fouad B <fouad709@gmail.com>
2016-03-22 19:24:52 +0300

    Update Plane.h

--- Plane.h
+++ Plane.h
@@ -8,5 +8,13 @@ class Plane:public Object {
 	double d;
 	vect3d direction;
 
+	Plane(vect3d dir_arg, double d_arg) {
 
+		d = d_arg;
+		direction = dir_arg;
+
+	}
+
+	Plane() {};
+	~Plane() {};
 };
