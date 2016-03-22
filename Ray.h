c5f653c9b522952b46a83638d8b5a8a54f1ca815
Fouad B <fouad709@gmail.com>
2016-03-22 19:08:29 +0300

    Create Ray.h

---
+++ Ray.h
@@ -0,0 +1,45 @@
+#pragma once 
+
+#include "Vect3d.h"
+
+class Ray {
+	
+	vect3d origin;
+	vect3d direction;
+
+public:
+
+// ===== Constructer =====
+	Ray() {
+
+		origin.x = 0;
+		origin.y = 0;
+		origin.z = 0;
+
+		direction.x = 0;
+		direction.y = 0;
+		direction.z = 1;
+	}
+	
+// ====== Constructer with parameters ======
+	Ray(vect3d newOrigin, vect3d newDirection) {
+
+		origin.x = newOrigin.x;
+		origin.y = newOrigin.y;
+		origin.z = newOrigin.z;
+
+		direction.x = newDirection.x;
+		direction.y = newDirection.y;
+		direction.z = newDirection.z;
+	}
+
+
+	void setOrigin(vect3d newOrigin);
+
+	void setDirection(vect3d newDirection);
+
+	vect3d getOrigin();
+
+	vect3d getDirection();
+
+};
