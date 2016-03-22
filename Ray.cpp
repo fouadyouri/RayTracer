6cbf0dc981464a28c286c8c190e64cbd769be312
Fouad B <fouad709@gmail.com>
2016-03-22 19:09:39 +0300

    Create Ray.cpp

---
+++ Ray.cpp
@@ -0,0 +1,24 @@
+#include "Ray.h"
+
+
+void Ray::setOrigin(vect3d newOrigin) {
+
+	origin.x = newOrigin.x;
+	origin.y = newOrigin.y;
+	origin.z = newOrigin.z;
+}
+
+void Ray::setDirection(vect3d newDirection) {
+	
+	direction.x = newDirection.x;
+	direction.y = newDirection.y;
+	direction.z = newDirection.z;
+}
+
+vect3d Ray::getOrigin() {
+	return origin;
+}
+
+vect3d Ray::getDirection() {
+	return direction;
+}
