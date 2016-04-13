973872e5952c754d4b953ea97cc2a623181fff45
Fouad B <fouad709@gmail.com>
2016-04-13 13:35:24 +0300

    Update Plane.h

--- Plane.h
+++ Plane.h
@@ -4,17 +4,42 @@
 
 
 class Plane:public Object {
-
-	double d;
-	vect3d direction;
-
-	Plane(vect3d dir_arg, double d_arg) {
-
-		d = d_arg;
-		direction = dir_arg;
-
-	}
+public:
+	double distance;
+	vect3d normal;
+	Color color;
 
 	Plane() {};
 	~Plane() {};
+
+	Plane(vect3d nor_arg, double dist_arg) {
+		distance = dist_arg;
+		normal = nor_arg;
+	}
+
+	vect3d getNormalAt(vect3d vect_arg) {
+		return normal;
+	}
+
+	double findIntersectionAt(Ray ray_arg) {
+		if (normal.dotProduct(ray_arg.getDirection()) == 0)
+			return -1;
+		else {
+			double t = -1.0 * (normal.dotProduct(ray_arg.getOrigin()) + distance) / (normal.dotProduct(ray_arg.getDirection()));
+			return t;
+		}
+	}
+
+	Color getColor() {
+		return color;
+	}
+
+	vect3d getNormal() {
+		return normal;
+	}
+
+	double getDistance() {
+		return distance;
+	}
+
 };
