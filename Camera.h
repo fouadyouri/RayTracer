f417fb6139ffa8bd42fd99a7e413fd4379b34946
Fouad B <fouad709@gmail.com>
2016-04-13 13:33:20 +0300

    Update Camera.h

--- Camera.h
+++ Camera.h
@@ -4,26 +4,41 @@
 class Camera {
 
 public:
-
-	vect3d camDown;
+	vect3d camUp;
 	vect3d camRight;
-	vect3d origin;
-	vect3d lookAt;
-
+	vect3d camPosition;
+	vect3d camDirection;
 	
-	Camera(vect3d camD, vect3d camR, vect3d orig, vect3d lookA) {
-		camDown = camD;
-		camRight = camR;
-		origin = orig;
-		lookAt = lookA;
+	Camera(vect3d cUp_var, vect3d cRight_var, vect3d cPosition_var, vect3d cDirection_var) {
+		camUp = cUp_var;
+		camRight = cRight_var;
+		camPosition = cPosition_var;
+		camDirection = cDirection_var;
 	}
 	
+	void setCamUp(vect3d position_var) {
+		camUp = position_var;
+	}
+	void setCamDirection(vect3d position_var) {
+		camDirection = position_var;
+	}
+	void setCamRight(vect3d position_var) {
+		camRight = position_var;
+	}
+	void setCamPosition(vect3d position_var) {
+		camPosition = position_var;
+	}
 	
-	vect3d getCamUp(vect3d position);
-	vect3d getCamDown(vect3d position);
-	vect3d getCamRight(vect3d position);
-	vect3d getCamLeft(vect3d position);
-	
-
-
+	vect3d getCamUp() {
+		return camUp;
+	}
+	vect3d getCamRight() {
+		return camRight;
+	}
+	vect3d getPosition() {
+		return camPosition;
+	}
+	vect3d getCamDirection() {
+		return camDirection;
+	}
 };
