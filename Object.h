78ff1aab01506b19f7d9e16ee969836a47240b08
Fouad B <fouad709@gmail.com>
2016-04-18 15:50:48 +0300

    Update Object.h

--- Object.h
+++ Object.h
@@ -4,13 +4,10 @@
 #include "Color.h"
 
 class Object {
-
 public:
 
 	virtual double findIntersectionAt(Ray r) = 0;
-
 	virtual Color getColor() = 0;
-
-	virtual vect3d getNormalAt(vect3d V) = 0
+	virtual vect3d getNormalAt(vect3d V) = 0;
 
 };
