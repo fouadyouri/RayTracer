62e4d78b81f79532d383da10506afab3e9418714
Fouad B <fouad709@gmail.com>
2016-04-18 15:49:42 +0300

    Update LightPointSource.h

--- LightPointSource.h
+++ LightPointSource.h
@@ -3,21 +3,18 @@
 #include "LightSource.h"
 
 class LightSourcePoint:public LightSource {
-
 	vect3d position;
 	Color color;
-	
+
 	vect3d getLightPosition();
-
 	Color getLightColor();
-
 };
 
 vect3d LightSourcePoint::getLightPosition() {
-
 	return position;
 }
 
 Color LightSourcePoint::getLightColor() {
 	return color;
 }
+
