d1202aa5ac790c5c8d123713a686c5b0b4bce683
Fouad B <fouad709@gmail.com>
2016-05-24 19:06:16 +0300

    Update Ray.cpp

--- Ray.cpp
+++ Ray.cpp
@@ -2,14 +2,12 @@
 
 
 void Ray::setOrigin(vect3d newOrigin) {
-
 	origin.x = newOrigin.x;
 	origin.y = newOrigin.y;
 	origin.z = newOrigin.z;
 }
 
 void Ray::setDirection(vect3d newDirection) {
-
 	direction.x = newDirection.x;
 	direction.y = newDirection.y;
 	direction.z = newDirection.z;
