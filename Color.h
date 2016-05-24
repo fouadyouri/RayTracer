26de5828cc2195b1adc05e617e6764cbdbc09a01
Fouad B <fouad709@gmail.com>
2016-05-24 19:04:23 +0300

    Update Color.h

--- Color.h
+++ Color.h
@@ -1,32 +1,43 @@
 #pragma once
 
 class Color {
+public:
 	double red;
 	double green;
 	double blue;
 
-public:
+	double special;
+
 // ===== Constructor =====
 	Color() {
 		red = 0;
 		green = 0;
 		blue = 0;
+		special = 0;
 	}
 
 // ====== Constructor with parameter ======
-	Color(double r, double g, double b) {
+	Color(double r, double g, double b, double s) {
 		red = r;
 		green = g;
 		blue = b;
+		special = s;
 	}
 
 	double getColorRed();
 	double getColorGreen();
 	double getColorBlue();
+    double getColorSpecial();
 
 	void setColorRed(double r);
 	void setColorGreen(double g);
 	void setColorBlue(double b);
+    void setColorSpecial(double s);
+
+    Color colorAdd(Color color_var);
+    Color colorMultiColor(Color color_var);
+    Color colorMultiDouble(double var);
+    Color normalized();
 
 };
 
